d6e3b7a938726b90e0dc14e0f05a60365a9c1966
Pamir Perdeci <pamirperdeci@gmail.com>
2023-03-13 06:16:02 -0400

    site basics added with static jsx

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
+  state = {
+    todos: []
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+    .then(res => {
+        this.setState({
+          ...this.state,
+          todos: []
+        })
+    })
+    .catch(err => {
+
+      
+    })
+
+  }
+
+
+  componentDidMount() {
+
+
+
+  }
   render() {
-    return null
+
+    return (
+      <div>
+    <div id="todos">
+      <h2>Todos:</h2>
+      <div>Walk The Dog</div>
+      <div>Take Out The Trash</div>  
+    </div>
+      <form id="todoForm">
+        <input type ="text" placeholder="Type todo"></input>
+        <input type="submit"></input>
+        <button>Clear completed</button>
+      </form>
+    </div>
+
+    )
   }
 }
