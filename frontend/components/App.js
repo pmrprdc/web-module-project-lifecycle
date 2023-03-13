ab4b2d5f03584f82438c4a89bd63b9622bafa26c
Pamir Perdeci <pamirperdeci@gmail.com>
2023-03-13 12:34:02 -0400

    Data from server is rendered inside app

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,7 +6,8 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
 
   state = {
-    todos: []
+    todos: [],
+    error: '',
   }
 
   fetchAllTodos = () => {
@@ -18,30 +19,43 @@ export default class App extends React.Component {
         })
     })
     .catch(err => {
-
+      
       
     })
 
   }
-
+  //helperfunction2fetch
+  fetchAllTodos = () => {
+    axios.get(URL).then(res => {
+      
+      this.setState({ ...this.state, todos: res.data.data })
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
 
   componentDidMount() {
-
-
+      // fetch all todos on server
+    this.fetchAllTodos()
 
   }
-  render() {
 
+
+  render() {
+  
     return (
       <div>
-    <div id="todos">
       <h2>Todos:</h2>
+    <div id="todos">
+      {this.state.todos.map(td=>{
+        return <div key={td.id}>{td.name}</div>
+      })}
       <div>Walk The Dog</div>
       <div>Take Out The Trash</div>  
     </div>
       <form id="todoForm">
         <input type ="text" placeholder="Type todo"></input>
-        <input type="submit"></input>
+        <input  type="submit"></input>
         <button>Clear completed</button>
       </form>
     </div>
