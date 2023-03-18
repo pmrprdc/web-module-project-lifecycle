1a781dc1b4128705e1e09d890172b1fdb2e8da9a
Pamir Perdeci <pamirperdeci@gmail.com>
2023-03-18 18:07:39 -0400

    Todo's are in their own component

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
-    return null
+    return (
+      <div onClick={this.props.toggleCompleted(this.props.id)} key={this.props.id}>{this.props.name}{this.props.completed? 'COMPLETE' : ''}</div>
+    )
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
   render() {
